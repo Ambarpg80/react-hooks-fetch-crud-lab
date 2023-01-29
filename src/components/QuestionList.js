b5860d3c642f893fa66174cd346d7d11013b6068
Ambar <Ambarpg80@gmail.com>
2023-01-29 13:45:57 -0500

    commit tree 9b1b9e2f67b6763b12ea7d82b55faf29aa544230

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
+import QuestionItem from './QuestionItem'
 
 function QuestionList() {
+  const [questionList, setQuestionList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(res => res.json())
+    .then( list => setQuestionList(list))
+  },[])
+
+  function handleQuestionDelete(id){
+    const deletedQuestion = questionList.filter(question => question.id !== id )
+    setQuestionList(deletedQuestion)
+  }
+  
+  function handleNewAnswer(updatedAnswer){
+    const newAnswer = questionList.map(question => {
+      if(question.id === updatedAnswer.id){
+        return updatedAnswer
+      }else {
+        return question
+      }
+    })
+      console.log(setQuestionList(newAnswer)) 
+  }
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList.map(question => <QuestionItem key={question.id} question={question} onDeleteQuestion={handleQuestionDelete}  onUpdate={handleNewAnswer}/>)}</ul>
     </section>
   );
 }
